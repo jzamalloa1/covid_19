0a2fb5b650d06dda4f62063914f42293f34350ab
JZ <jzamalloa1@gmail.com>
2020-07-07 21:12:28 -0700

    updated filename source from google and started basic tracking plot for both US and ex-US

--- app.R
+++ app.R
@@ -15,27 +15,121 @@ library(directlabels)
 
 all_data <- fread("https://open-covid-19.github.io/data/v2/master.csv")
 
+all_data <- fread("https://storage.googleapis.com/covid19-open-data/v2/main.csv")
+
+all_data %>%
+  mutate(day = as.Date(date, format= "%Y-%m-%d")) %>%
+  filter(subregion1_code=="LIM") %>%
+  arrange(day) %>%
+  as.data.table()
+
+
 prep_data <- all_data %>%
   mutate(day = as.Date(date, format= "%Y-%m-%d"), 
          country_name=replace(country_name, country_name=="United States of America", "US")) %>%
   filter(subregion1_name=="" & subregion2_name=="", aggregation_level==0) %>%
-  distinct(key, country_name, day, new_confirmed, new_deceased, total_confirmed, total_deceased, 
-           population, population_density, male_population, female_population,
+  distinct(key, country_name, day, new_confirmed, new_deceased, total_confirmed, total_deceased,
+           new_tested, total_tested,
+           population, population_density, population_male, population_female,
            human_development_index, diabetes_prevalence, smoking_prevalence, comorbidity_mortality_rate, health_expenditure
-           ) %>%
+  ) %>%
   
   # Get daily smoothed cases, deaths over 7 days
   arrange(country_name, day) %>%
   group_by(country_name) %>%
   mutate(s_new_confirmed = roll_mean(new_confirmed, n=7, align = "right", fill = NA),
-         s_new_death = roll_mean(new_deceased, n=7, align="right", fill=NA)) %>%
+         s_new_deceased = roll_mean(new_deceased, n=7, align="right", fill=NA)) %>%
   
   # Binarize Developed status by HDI
   mutate(Developed = human_development_index>0.846) %>%
   
   # Get incidence
-  mutate(new_case_inc = s_new_confirmed/population, new_death_inc = s_new_death/population) %>%
+  mutate(new_case_inc = s_new_confirmed/population, new_death_inc = s_new_deceased/population) %>%
   as.data.table()
+
+
+prep_data_l1 <- all_data %>%
+  mutate(day = as.Date(date, format= "%Y-%m-%d"), 
+         country_name=replace(country_name, country_name=="United States of America", "US")) %>%
+  filter(aggregation_level==1) %>%
+  distinct(key, country_name, subregion1_code, subregion1_name,
+           day, new_confirmed, new_deceased, total_confirmed, total_deceased, 
+           new_tested, total_tested,
+           population
+  ) %>%
+  
+  # Filter out negative value
+  mutate(new_deceased = replace(new_deceased, new_deceased<0, 0)) %>%
+  
+  # Get daily smoothed cases, deaths over 7 days
+  arrange(country_name, subregion1_code, day) %>%
+  group_by(country_name, subregion1_code) %>%
+  mutate(s_new_confirmed = roll_mean(new_confirmed, n=7, align = "right", fill = NA),
+         s_new_deceased = roll_mean(new_deceased, n=7, align="right", fill=NA)) %>%
+  
+  # Get incidence
+  mutate(new_case_inc = s_new_confirmed/population, new_death_inc = s_new_deceased/population) %>%
+  as.data.table()
+
+function_plot_region <-  function(x, country, level_1=""){
+  
+  x <- x %>%
+    filter(day>"2020-03-01") %>%
+    filter(country_name==country) %>%
+    as.data.table()
+  
+  if (level_1!=""){
+    x <- x %>%
+      filter(subregion1_name==level_1) %>%
+      as.data.table()
+  }
+  
+  bars  <- x %>%
+    mutate(new_deceased = new_deceased*10) %>%
+    distinct(day, new_confirmed, new_deceased) %>%
+    melt(id.vars=c("day")) %>%
+    as.data.table()
+  
+  lines <- x %>%
+    mutate(s_new_deceased = s_new_deceased*10) %>%
+    distinct(day, s_new_confirmed, s_new_deceased) %>%
+    melt(id.vars=c("day")) %>%
+    as.data.table()
+  
+  p <- ggplot() +
+    geom_bar(data=bars, aes(day, value, fill=variable), stat="identity", position="dodge", alpha=0.7) +
+    
+    geom_line(data=lines[variable=="s_new_confirmed"], aes(day, value), color="darkblue", size=1.5) +
+    geom_line(data=lines[variable=="s_new_deceased"], aes(day, value), color="darkred", size=1.5) +
+    
+    geom_line(data=lines[variable=="s_new_confirmed"], aes(day, value/10, color="1% Mortality"), 
+              color="darkred", linetype="dashed", size=1.2) +
+    
+    geom_dl(data=lines[variable=="s_new_confirmed"], aes(day, value/10, label="1% Mortal"),
+            method = list(dl.combine("last.points"), cex = 1, hjust=0, vjust=0.1)) +
+    
+    scale_fill_manual(name="Events", values = c("darkblue", "darkred"), labels=c("Cases", "Deaths")) +
+    scale_y_continuous(sec.axis = sec_axis(~./10, name="New Deaths")) +
+    theme_minimal() +
+    theme(axis.title=element_text(size=16), axis.text = element_text(size=13),
+          legend.title = element_text(size=14), legend.text=element_text(size=12)) +
+    xlab("Date") + ylab("New Cases")
+  
+  return(p)
+}
+
+function_plot_region(prep_data, "US")
+function_plot_region(prep_data_l1, "US", "California")
+function_plot_region(prep_data_l1, "Peru", "Lima")
+
+prep_data_l1 %>%
+  filter(country_name=="US") %>%
+  filter(subregion1_name=="Nevada") %>%
+  mutate(trend_14_day_perc =  ((s_new_confirmed / lag(s_new_confirmed, n = 13)) - 1)*100 ) %>%
+  filter(day>"2020-06-23") %>%
+  as.data.table()
+
+
 
 
 p <- prep_data %>%
